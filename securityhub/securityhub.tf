39ff25cf3b62fcc0692fad00251065def3383aee
Pavel Kargin <pav.kar97@gmail.com>
2024-05-13 19:04:12 +0300

    securityhub enable all standards, enable in specified regions, auto enable in new accounts

--- securityhub/securityhub.tf
+++ securityhub/securityhub.tf
@@ -6,3 +6,10 @@ resource "aws_securityhub_finding_aggregator" "monitored_regions" {
 resource "aws_securityhub_organization_configuration" "master" {
   auto_enable = true
 }
+
+# Enable all standards
+resource "aws_securityhub_standards_subscription" "standards_subscription" {
+  for_each = { for standard in var.standards_arns : standard => standard }
+
+  standards_arn = each.value
+}
--- securityhub/variables.tf
+++ securityhub/variables.tf
@@ -1,1 +1,19 @@
-variable "aws_regions" {}
+variable "aws_regions" {
+  description = "The list of AWS regions to enable Security Hub"
+  type        = list(string)
+  default = [
+    "eu-central-1",
+  ]
+}
+
+variable "standards_arns" {
+type = list(string)
+    description = "The list of standards ARNs to enable"
+  default = [
+    "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.2.0",
+    "arn:aws:securityhub:eu-central-1::standards/cis-aws-foundations-benchmark/v/1.4.0",
+    "arn:aws:securityhub:eu-central-1::standards/aws-foundational-security-best-practices/v/1.0.0",
+    "arn:aws:securityhub:eu-central-1::standards/pci-dss/v/3.2.1",
+    "arn:aws:securityhub:eu-central-1::standards/nist-800-53/v/5.0.0"
+  ]
+}
