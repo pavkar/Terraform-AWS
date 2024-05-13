95ce3faac448d84885f204ad1389323de19f24c9
Pavel Kargin <pav.kar97@gmail.com>
2024-05-13 19:13:34 +0300

    example main module

---
+++ main.tf
@@ -0,0 +1,30 @@
+module "securityhub" {
+  source = "securityhub"
+}
+
+module "cloudtrail" {
+  source = "cloudtrail"
+}
+
+module "iam" {
+  source = "iam"
+}
+
+module "organization" {
+  source = "organization"
+
+  # Needs to be changed for correct values
+  organization_admin_role = "AdminRole"
+  root_account = "123456789"
+}
+
+module "ec2" {
+  source = "ec2"
+
+  # Needs to be changed for correct values
+  key_name = "my-key"
+  ami_id = "ami-1234556789"
+  ssh_cidr_block = ""
+  http_cidr_block = ""
+  egress_cidr_block = ""
+}
