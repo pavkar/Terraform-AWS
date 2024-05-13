7605872f67c9373b732f8eebac1c64fb825a24ea
Pavel Kargin <pav.kar97@gmail.com>
2024-05-13 14:46:04 +0300

    Adding check for admin role, updating policy scopes

--- organization/scp.tf
+++ organization/scp.tf
@@ -1,72 +1,71 @@
 # Disabling security hub (except for the root account)
 data "aws_iam_policy_document" "block_disable_securityhub" {
   statement {
-    sid       = "DenyDisableSecurityHub"
-    effect    = "Deny"
-    actions   = [
-      "securityhub:DisableSecurityHub"
+    sid    = "DenyDisableSecurityHub"
+    effect = "Deny"
+    actions = [
+      "securityhub:DeleteInvitations",
+      "securityhub:DisableSecurityHub",
+      "securityhub:DisassociateFromMasterAccount",
+      "securityhub:DeleteMembers",
+      "securityhub:DisassociateMembers"
     ]
     resources = ["*"]
 
     condition {
       test     = "ArnNotEquals"
       variable = "aws:PrincipalArn"
-      values   = ["arn:aws:iam::<account-id>:root"]
+      values   = ["arn:aws:iam::${var.root_account}:root", "arn:aws:iam::${var.root_account}:role/${var.organization_admin_role}"]
     }
   }
 }
 
 resource "aws_organizations_policy" "block_disable_securityhub" {
-  name     = "block_disable_securityhub"
-  content  = data.aws_iam_policy_document.block_disable_securityhub.json
-  type     = "SERVICE_CONTROL_POLICY"
+  name    = "block_disable_securityhub"
+  content = data.aws_iam_policy_document.block_disable_securityhub.json
+  type    = "SERVICE_CONTROL_POLICY"
 }
 
 # Disabling CloudTrail (except for the root account)
 data "aws_iam_policy_document" "block_disable_cloudtrail" {
   statement {
-    sid       = "DenyDisableCloudTrail"
-    effect    = "Deny"
-    actions   = [
-      "cloudtrail:StopLogging"
+    sid    = "DenyDisableCloudTrail"
+    effect = "Deny"
+    actions = [
+      "cloudtrail:StopLogging",
+      "cloudtrail:DeleteTrail"
     ]
     resources = ["*"]
 
     condition {
       test     = "ArnNotEquals"
       variable = "aws:PrincipalArn"
-      values   = ["arn:aws:iam::<account-id>:root"]
+      values   = ["arn:aws:iam::${var.root_account}:root", "arn:aws:iam::${var.root_account}:role/${var.organization_admin_role}"]
     }
   }
 }
 
 resource "aws_organizations_policy" "block_disable_cloudtrail" {
-  name     = "block_disable_cloudtrail"
-  content  = data.aws_iam_policy_document.block_disable_cloudtrail.json
-  type     = "SERVICE_CONTROL_POLICY"
+  name    = "block_disable_cloudtrail"
+  content = data.aws_iam_policy_document.block_disable_cloudtrail.json
+  type    = "SERVICE_CONTROL_POLICY"
 }
 
 # Exposing CloudTrail S3 bucket publicly
 data "aws_iam_policy_document" "block_public_cloudtrail" {
   statement {
-    sid       = "DenyPublicAccessToCloudTrailBucket"
-    effect    = "Deny"
-    actions   = [
-      "s3:PutBucketAcl",
-      "s3:PutBucketPolicy"
+    sid    = "DenyPublicAccessToCloudTrailBucket"
+    effect = "Deny"
+    actions = [
+      "s3:PutBucketPublicAccessBlock",
+      "s3:PutAccountPublicAccessBlock"
     ]
     resources = ["arn:aws:s3:::*"]
-
-    condition {
-      test     = "StringEquals"
-      variable = "s3:x-amz-acl"
-      values   = ["public-read", "public-read-write"]
-    }
   }
 }
 
 resource "aws_organizations_policy" "block_public_cloudtrail" {
-  name     = "block_public_cloudtrail"
-  content  = data.aws_iam_policy_document.block_public_cloudtrail.json
-  type     = "SERVICE_CONTROL_POLICY"
+  name    = "block_public_cloudtrail"
+  content = data.aws_iam_policy_document.block_public_cloudtrail.json
+  type    = "SERVICE_CONTROL_POLICY"
 }
---
+++ organization/variables.tf
@@ -0,0 +1,7 @@
+variable "root_account" {
+  description = "The root account ID"
+}
+
+variable "organization_admin_role" {
+  description = "The name of the organization admin role"
+}
