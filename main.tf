d616b3dbbe4592ed43e0f184640796bffcf8ad43
Pavel Kargin <pav.kar97@gmail.com>
2024-05-13 20:15:06 +0300

    reformat main file

--- main.tf
+++ main.tf
@@ -15,16 +15,16 @@ module "organization" {
 
   # Needs to be changed for correct values
   organization_admin_role = "AdminRole"
-  root_account = "123456789"
+  root_account            = "123456789"
 }
 
 module "ec2" {
   source = "ec2"
 
   # Needs to be changed for correct values
-  key_name = "my-key"
-  ami_id = "ami-1234556789"
-  ssh_cidr_block = ""
-  http_cidr_block = ""
+  key_name          = "my-key"
+  ami_id            = "ami-1234556789"
+  ssh_cidr_block    = ""
+  http_cidr_block   = ""
   egress_cidr_block = ""
 }
